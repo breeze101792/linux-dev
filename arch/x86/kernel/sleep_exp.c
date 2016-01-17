2a29c3c2fedf3d0c42553b842846515b0d7dca78
Shaowu <breeze101792@gmail.com>
2016-01-17 17:59:24 +0000

    use static wq to add process to wq

--- arch/x86/kernel/sleep_exp.c
+++ arch/x86/kernel/sleep_exp.c
@@ -1,16 +1,23 @@
 #include <linux/linkage.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/mm_types.h>
-#include <linux/mm.h>
-#include <asm/page.h>
-#include <linux/pid.h>
-#include <linux/file.h>
-#include <linux/fs.h>
 #include <linux/slab.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
+static wait_queue_head_t wq;
+static int queue_flag = 1;
+	
 
 asmlinkage long sys_go_to_sleep_now() {
 	printk("go_to_sleep_now");
+	int sleep_flag = 0;
+	if(queue_flag)
+	{
+		queue_flag = 0;
+		init_waitqueue_head(&wq);
+		printk("wq define");
+	}
+	wait_event_interruptible(wq, sleep_flag);
+	printk("finished!\n");
         return 0;
 }
 asmlinkage long sys_wake_up_my_process() {
